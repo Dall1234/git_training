7829128b70b52f5d6ed15f34fbe37f6a03e14768
Dall1234 <201909837@post.au.dk>
2024-09-20 13:15:25 +0200

    Update test_packages.R

--- test_packages.R
+++ test_packages.R
@@ -1,11 +1,5 @@
 # Load necessary packages
 library(dplyr)
 library(ggplot2)
-<<<<<<< HEAD
-#some comment, with exstra explanation
-
-
-=======
-#SOME COMMIT asd
->>>>>>> 7d6884e2651aa4c10f42ef03710d10f781b66ce7
+#some comment, with Exstra
 
